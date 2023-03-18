317b736cb6d2dffa0f6e0e4c133cea29883b4566
Sergii Podriezienko <podreenko1986@gmail.com>
2023-03-18 17:30:38 +0200

    changes new
    changes new

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,104 +1,106 @@
-import React, { Component } from 'react';
+import React, { Component, useState } from 'react';
 import Button from './Button/Button';
 import Searchbar from './Searchbar/Searchbar';
 import ImageGallery from './ImageGallery/ImageGallery';
 import Loader from './Loader/Loader';
 
-class App extends Component {
-  state = {
-    imageArray: [],
-    loadStatus: false,
-    searchKey: '',
-    dataFilled: false,
-    countOfPagePagination: 12,
-  };
+const App = () => {};
 
-  page = 1;
+//class App extends Component {
+//  state = {
+//    imageArray: [],
+//   loadStatus: false,
+//   searchKey: '',
+//   dataFilled: false,
+//   countOfPagePagination: 12,
+// };
 
-  getApi = (imageName, pageNumber) => {
-    return (
-      'https://pixabay.com/api/?q=' +
-      imageName +
-      '&page=' +
-      pageNumber +
-      '&key=33160634-a3b69315a0080f1036a2567f6&image_type=photo&orientation=horizontal&per_page=' +
-      this.state.countOfPagePagination
-    );
-  };
+// page = 1;
 
-  getOtherImageCount = (totalHits, pageCount) => {
-    return totalHits - pageCount * this.state.countOfPagePagination > 0;
-  };
+// getApi = (imageName, pageNumber) => {
+//   return (
+//     'https://pixabay.com/api/?q=' +
+//     imageName +
+//     '&page=' +
+//     pageNumber +
+//     '&key=33160634-a3b69315a0080f1036a2567f6&image_type=photo&orientation=horizontal&per_page=' +
+//     this.state.countOfPagePagination
+//  );
+// };
 
-  initDate = data => {
-    this.setState({ imageArray: [] });
-    this.setState({ searchKey: data.imageName });
-  };
+// getOtherImageCount = (totalHits, pageCount) => {
+//   return totalHits - pageCount * this.state.countOfPagePagination > 0;
+// };
 
-  setValues = (data, loadMore) => {
-    if (loadMore) {
-      this.setState(prevState => ({
-        imageArray: [...prevState.imageArray, ...data.hits],
-      }));
-    } else {
-      this.setState({ imageArray: data.hits });
-    }
-    this.setState({
-      dataFilled: this.getOtherImageCount(data.totalHits, this.page),
-    });
-  };
+//initDate = data => {
+//  this.setState({ imageArray: [] });
+//  this.setState({ searchKey: data.imageName });
+//};
 
-  processData = (imageName, loadMore) => {
-    this.setState({ loadStatus: true });
+// setValues = (data, loadMore) => {
+//   if (loadMore) {
+//     this.setState(prevState => ({
+//      imageArray: [...prevState.imageArray, ...data.hits],
+//    }));
+//   } else {
+//     this.setState({ imageArray: data.hits });
+//   }
+//   this.setState({
+//     dataFilled: this.getOtherImageCount(data.totalHits, this.page),
+//   });
+// };
 
-    const URL = this.getApi(imageName, this.page);
+// processData = (imageName, loadMore) => {
+//  this.setState({ loadStatus: true });
 
-    fetch(URL)
-      .then(response => {
-        if (response.ok) {
-          return response.json();
-        }
-        return Promise.reject(new Error(`Немає даних по запиту ${imageName}`));
-      })
-      .then(data => {
-        this.setValues(data, loadMore);
-      })
-      .catch(error => {})
-      .finally(() => {
-        this.setState({ loadStatus: false });
-      });
-  };
+//  const URL = this.getApi(imageName, this.page);
 
-  formSubmitHandler = data => {
-    this.page = 1;
-    this.initDate(data);
-    this.processData(data.imageName, false);
-  };
+// fetch(URL)
+//   .then(response => {
+//     if (response.ok) {
+//       return response.json();
+//     }
+//     return Promise.reject(new Error(`Немає даних по запиту ${imageName}`));
+//   })
+//   .then(data => {
+//    this.setValues(data, loadMore);
+//  })
+//  .catch(error => {})
+//  .finally(() => {
+//    this.setState({ loadStatus: false });
+//  });
+//};
 
-  buttonLoadClick = data => {
-    if (this.state.searchKey.trim() !== '') {
-      this.page += 1;
-      this.processData(this.state.searchKey, true);
-    }
-  };
+//formSubmitHandler = data => {
+//  this.page = 1;
+//  this.initDate(data);
+//  this.processData(data.imageName, false);
+//};
 
-  render() {
-    return (
-      <div
-        style={{
-          display: 'grid',
-          gridTemplateColumns: '1fr',
-          gridGap: '16px',
-          paddingBottom: '24px',
-        }}
-      >
-        <Searchbar onSubmit={this.formSubmitHandler} />
-        <ImageGallery images={this.state.imageArray} />
-        {this.state.loadStatus && <Loader />}
-        {this.state.dataFilled && <Button onSubmit={this.buttonLoadClick} />}
-      </div>
-    );
-  }
-}
+//buttonLoadClick = data => {
+//  if (this.state.searchKey.trim() !== '') {
+//    this.page += 1;
+//    this.processData(this.state.searchKey, true);
+// }
+//};
+
+//render() {
+// return (
+//  <div
+//   style={{
+//    display: 'grid',
+//    gridTemplateColumns: '1fr',
+//    gridGap: '16px',
+//  paddingBottom: '24px',
+// }}
+// >
+//  <Searchbar onSubmit={this.formSubmitHandler} />
+// <ImageGallery images={this.state.imageArray} />
+// {this.state.loadStatus && <Loader />}
+// {this.state.dataFilled && <Button onSubmit={this.buttonLoadClick} />}
+//</div>
+//);
+// }
+//}
 
 export default App;
--- src/components/Searchbar/Searchbar.js
+++ src/components/Searchbar/Searchbar.js
@@ -38,45 +38,6 @@ const Searchbar = ({ onSubmit }) => {
   );
 };
 
-//class Searchbar extends Component {
-//  state = {
-//    imageName: '',
-//  };
-
-// handleChange = e => {
-//   const { name, value } = e.target;
-//   this.setState({ [name]: value });
-// };
-
-// handleSubmit = evt => {
-//   evt.preventDefault();
-//  this.props.onSubmit(this.state);
-// };
-
-// render() {
-//  return (
-//     <header className={css.searchbar}>
-//       <form className={css.searchForm} onSubmit={this.handleSubmit}>
-//        <button className={css.searchFormButton} type="submit">
-//           <AiOutlineSearch className={css.icon} size={16} />
-//          <span className={css.buttonLabel}>Search</span>
-//        </button>
-//        <input
-//          className={css.searchFormInput}
-//          value={this.state.imageName}
-//          onChange={this.handleChange}
-//         type="text"
-//          name="imageName"
-//         autoComplete="off"
-//         autoFocus
-//         placeholder="Search images and photos"
-//       />
-//     </form>
-//   </header>
-//  );
-// }
-//}
-
 Searchbar.propTypes = {
   onSubmit: PropTypes.func.isRequired,
 };
