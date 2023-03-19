7807679fc5a0ccfc7ee1b289eb3e6a7402a971b5
Sergii Podriezienko <podreenko1986@gmail.com>
2023-03-19 10:42:23 +0200

    finish changes
    finish changes

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,106 +1,96 @@
-import React, { Component, useState } from 'react';
+import React, { useState } from 'react';
 import Button from './Button/Button';
 import Searchbar from './Searchbar/Searchbar';
 import ImageGallery from './ImageGallery/ImageGallery';
 import Loader from './Loader/Loader';
 
-const App = () => {};
+let page = 1;
 
-//class App extends Component {
-//  state = {
-//    imageArray: [],
-//   loadStatus: false,
-//   searchKey: '',
-//   dataFilled: false,
-//   countOfPagePagination: 12,
-// };
+const App = () => {
+  const [imageArray, SetImageArray] = useState([]);
+  const [loadStatus, SetLoadStatus] = useState(false);
+  const [searchKey, SetSearchKey] = useState('');
+  const [dataFilled, SetDataFilled] = useState(false);
 
-// page = 1;
+  const countOfPagePagination = 12;
 
-// getApi = (imageName, pageNumber) => {
-//   return (
-//     'https://pixabay.com/api/?q=' +
-//     imageName +
-//     '&page=' +
-//     pageNumber +
-//     '&key=33160634-a3b69315a0080f1036a2567f6&image_type=photo&orientation=horizontal&per_page=' +
-//     this.state.countOfPagePagination
-//  );
-// };
+  const getApi = (imageName, pageNumber) => {
+    return (
+      'https://pixabay.com/api/?q=' +
+      imageName +
+      '&page=' +
+      pageNumber +
+      '&key=33160634-a3b69315a0080f1036a2567f6&image_type=photo&orientation=horizontal&per_page=' +
+      countOfPagePagination
+    );
+  };
 
-// getOtherImageCount = (totalHits, pageCount) => {
-//   return totalHits - pageCount * this.state.countOfPagePagination > 0;
-// };
+  const getOtherImageCount = (totalHits, pageCount) => {
+    return totalHits - pageCount * countOfPagePagination > 0;
+  };
 
-//initDate = data => {
-//  this.setState({ imageArray: [] });
-//  this.setState({ searchKey: data.imageName });
-//};
+  const initDate = data => {
+    SetImageArray(state => []);
+    SetSearchKey(state => data.imageName);
+  };
 
-// setValues = (data, loadMore) => {
-//   if (loadMore) {
-//     this.setState(prevState => ({
-//      imageArray: [...prevState.imageArray, ...data.hits],
-//    }));
-//   } else {
-//     this.setState({ imageArray: data.hits });
-//   }
-//   this.setState({
-//     dataFilled: this.getOtherImageCount(data.totalHits, this.page),
-//   });
-// };
+  const setValues = (data, loadMore) => {
+    if (loadMore) {
+      SetImageArray(state => [...state, ...data.hits]);
+    } else {
+      SetImageArray(state => data.hits);
+    }
+    SetDataFilled(state => getOtherImageCount(data.totalHits, page));
+  };
 
-// processData = (imageName, loadMore) => {
-//  this.setState({ loadStatus: true });
+  const processData = (imageName, loadMore) => {
+    SetLoadStatus(state => true);
+    const URL = getApi(imageName, page);
 
-//  const URL = this.getApi(imageName, this.page);
+    fetch(URL)
+      .then(response => {
+        if (response.ok) {
+          return response.json();
+        }
+        return Promise.reject(new Error(`Немає даних по запиту ${imageName}`));
+      })
+      .then(data => {
+        setValues(data, loadMore);
+      })
+      .catch(error => {})
+      .finally(() => {
+        SetLoadStatus(state => false);
+      });
+  };
 
-// fetch(URL)
-//   .then(response => {
-//     if (response.ok) {
-//       return response.json();
-//     }
-//     return Promise.reject(new Error(`Немає даних по запиту ${imageName}`));
-//   })
-//   .then(data => {
-//    this.setValues(data, loadMore);
-//  })
-//  .catch(error => {})
-//  .finally(() => {
-//    this.setState({ loadStatus: false });
-//  });
-//};
+  const formSubmitHandler = data => {
+    page = 1;
+    initDate(data);
+    processData(data.imageName, false);
+  };
 
-//formSubmitHandler = data => {
-//  this.page = 1;
-//  this.initDate(data);
-//  this.processData(data.imageName, false);
-//};
+  const buttonLoadClick = data => {
+    if (searchKey.trim() !== '') {
+      page += 1;
+      processData(searchKey, true);
+    }
+  };
 
-//buttonLoadClick = data => {
-//  if (this.state.searchKey.trim() !== '') {
-//    this.page += 1;
-//    this.processData(this.state.searchKey, true);
-// }
-//};
-
-//render() {
-// return (
-//  <div
-//   style={{
-//    display: 'grid',
-//    gridTemplateColumns: '1fr',
-//    gridGap: '16px',
-//  paddingBottom: '24px',
-// }}
-// >
-//  <Searchbar onSubmit={this.formSubmitHandler} />
-// <ImageGallery images={this.state.imageArray} />
-// {this.state.loadStatus && <Loader />}
-// {this.state.dataFilled && <Button onSubmit={this.buttonLoadClick} />}
-//</div>
-//);
-// }
-//}
+  return (
+    <div
+      style={{
+        display: 'grid',
+        gridTemplateColumns: '1fr',
+        gridGap: '16px',
+        paddingBottom: '24px',
+      }}
+    >
+      <Searchbar onSubmit={formSubmitHandler} />
+      <ImageGallery images={imageArray} />
+      {loadStatus && <Loader />}
+      {dataFilled && <Button onSubmit={buttonLoadClick} />}
+    </div>
+  );
+};
 
 export default App;
